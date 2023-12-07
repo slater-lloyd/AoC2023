6daab5128dba1f2361a3d87ecef4f62f13d36c46
SlaterL <slater@slater3.0>
2023-12-06 23:31:05 -0700

    day7 part1

---
+++ day7/part2.py
@@ -0,0 +1,123 @@
+from functools import cmp_to_key
+
+input = []
+
+with open("day7/test.txt", "r") as f:
+    input = f.read().splitlines()
+
+
+def parse(line):
+    return line.split()
+
+
+def compare(x, y):
+    xVal = getVal(x)
+    yVal = getVal(y)
+    if xVal > yVal:
+        return 1
+    elif yVal > xVal:
+        return -1
+    else:
+        for i in range(5):
+            xInt = cardToInt(x[i])
+            yInt = cardToInt(y[i])
+            if xInt > yInt:
+                return 1
+            elif yInt > xInt:
+                return -1
+    return 0
+
+
+def cardToInt(card):
+    cards = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+    return 13-cards.index(card)
+
+
+def getVal(hand):
+    if isFiveOfKind(hand):
+        return 6
+    elif isFourOfKind(hand):
+        return 5
+    elif isFullHouse(hand):
+        return 4
+    elif isThreeOfKind(hand):
+        return 3
+    elif isTwoPair(hand):
+        return 2
+    elif isPair(hand):
+        return 1
+    return 0
+
+
+def isFiveOfKind(hand):
+    for card in hand:
+        if hand.count(card) == 5 or (hand.count(card)+hand.count("J") == 5 and card != "J"):
+            return True
+    return False
+
+
+def isFourOfKind(hand):
+    for card in hand:
+        if hand.count(card) == 4 or (hand.count(card)+hand.count("J") == 4 and card != "J"):
+            return True
+    return False
+
+
+def isFullHouse(hand):
+    tempHand = hand
+    for card in tempHand:
+        if tempHand.count(card) == 2:
+            tempHand.replace(card, "")
+            break
+
+    for card in tempHand:
+        if tempHand.count(card) == 3:
+            return True
+        elif tempHand.count(card) == 2 and "J" in tempHand:
+            return True
+    return False
+
+
+def isThreeOfKind(hand):
+    for card in hand:
+        if hand.count(card) == 3 or (hand.count(card)+hand.count("J") == 3 and card != "J"):
+            return True
+    return False
+
+
+def isTwoPair(hand):
+    tempHand = hand
+    for card in tempHand:
+        if tempHand.count(card) == 2:
+            tempHand.replace(card, "")
+            break
+
+    for card in tempHand:
+        if tempHand.count(card) == 2 or ("J" in tempHand and card != "J"):
+            return True
+    return False
+
+
+def isPair(hand):
+    for card in hand:
+        if hand.count(card) == 2 or (hand.count(card)+hand.count("J") == 2 and card != "J"):
+            if "J" in hand:
+                print(f"Pair: {hand}")
+            return True
+    return False
+
+
+def main():
+    hands = [parse(line) for line in input]
+    hands = sorted(hands, key=cmp_to_key(
+        lambda x, y: compare(x[0], y[0])))
+
+    sum = 0
+    for i, hand in enumerate(hands):
+        sum += int(hand[1])*(i+1)
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
